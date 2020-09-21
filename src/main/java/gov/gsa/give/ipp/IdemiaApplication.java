e7cc4fccff080263fcd0fd7ee43f354406760ecd
shumatep <pshumate@easydynamics.com>
2020-09-21 15:10:53 -0400

    initial spring boot skeleton

---
+++ src/main/java/gov/gsa/give/ipp/IdemiaApplication.java
@@ -0,0 +1,13 @@
+package gov.gsa.give.ipp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdemiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdemiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gov/gsa/give/ipp/IdemiaApplicationTests.java
@@ -0,0 +1,13 @@
+package gov.gsa.give.ipp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdemiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
