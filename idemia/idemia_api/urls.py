ba5e5445edacb7ea9754af5ce699ed4e64595bd6
folksgl <Gfolks14@gmail.com>
2021-01-13 13:26:43 -0500

    add 'name' fields to the url paths for referencing with 'reverse()'

--- idemia/idemia_api/urls.py
+++ idemia/idemia_api/urls.py
@@ -6,8 +6,12 @@ from . import views
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path("enrollment/", views.EnrollmentRecordCreate.as_view()),
-    path("enrollment/<uuid:pk>", views.EnrollmentRecordDetail.as_view()),
+    path("enrollment/", views.EnrollmentRecordCreate.as_view(), name="enrollment"),
+    path(
+        "enrollment/<uuid:pk>",
+        views.EnrollmentRecordDetail.as_view(),
+        name="enrollment-record",
+    ),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
 
