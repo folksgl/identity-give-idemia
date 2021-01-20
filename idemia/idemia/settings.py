b251e689ba04ace5e6b224e6e7d98dbb1e243d62
folksgl <Gfolks14@gmail.com>
2021-01-20 15:53:32 -0500

    remove prefix from secret key environment name

--- idemia/idemia/settings.py
+++ idemia/idemia/settings.py
@@ -27,7 +27,7 @@ STATICFILES_DIRS = (os.path.join(PROJECT_DIR, "static"),)
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Default to False unless environment variable is present
