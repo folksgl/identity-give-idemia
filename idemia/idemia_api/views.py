0a7e1dafdbc3b5d5bbc77eae580f8ff79867c45c
folksgl <Gfolks14@gmail.com>
2021-01-20 17:49:25 -0500

    override mixin methods for interacting with EnrollmentRecord objects

--- idemia/idemia_api/views.py
+++ idemia/idemia_api/views.py
@@ -1,18 +1,46 @@
 """ Views for Idemia API """
-from rest_framework import generics
+import logging
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework import status
 from .models import EnrollmentRecord
 from .serializers import EnrollmentRecordSerializer
 
+logging.basicConfig(level=logging.INFO)
 
-class EnrollmentRecordCreate(generics.CreateAPIView):
+
+class EnrollmentRecordCreate(ListCreateAPIView):
     """ Create EnrollmentRecord objects """
 
     queryset = EnrollmentRecord.objects.all()
     serializer_class = EnrollmentRecordSerializer
 
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        if response.status_code == status.HTTP_201_CREATED:
+            logging.info("Enrollment Record Created")
+        return response
 
-class EnrollmentRecordDetail(generics.RetrieveUpdateDestroyAPIView):
+
+class EnrollmentRecordDetail(RetrieveUpdateDestroyAPIView):
     """ Perform read, update, delete operations on EnrollmentRecord objects """
 
     queryset = EnrollmentRecord.objects.all()
     serializer_class = EnrollmentRecordSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        response = super().retrieve(request, *args, **kwargs)
+        if response.status_code == status.HTTP_200_OK:
+            logging.info("Enrollment Record Retrieved")
+        return response
+
+    def update(self, request, *args, **kwargs):
+        response = super().update(request, *args, **kwargs)
+        if response.status_code == status.HTTP_200_OK:
+            logging.info("Enrollment Record Updated")
+        return response
+
+    def destroy(self, request, *args, **kwargs):
+        response = super().destroy(request, *args, **kwargs)
+        if response.status_code == status.HTTP_204_NO_CONTENT:
+            logging.info("Enrollment Record Deleted")
+        return response
