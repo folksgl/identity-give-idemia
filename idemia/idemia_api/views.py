d987e4b048cc0ffbe097ce0a774a21a7f37c0bd8
folksgl <Gfolks14@gmail.com>
2021-01-21 11:40:48 -0500

    default to logging config in settings.py

--- idemia/idemia_api/views.py
+++ idemia/idemia_api/views.py
@@ -1,14 +1,15 @@
 """ Views for Idemia API """
 import logging
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import (
+    CreateAPIView,
+    RetrieveUpdateDestroyAPIView,
+)
 from rest_framework import status
 from .models import EnrollmentRecord
 from .serializers import EnrollmentRecordSerializer
 
-logging.basicConfig(level=logging.INFO)
 
-
-class EnrollmentRecordCreate(ListCreateAPIView):
+class EnrollmentRecordCreate(CreateAPIView):
     """ Create EnrollmentRecord objects """
 
     queryset = EnrollmentRecord.objects.all()
@@ -17,7 +18,7 @@ class EnrollmentRecordCreate(ListCreateAPIView):
     def create(self, request, *args, **kwargs):
         response = super().create(request, *args, **kwargs)
         if response.status_code == status.HTTP_201_CREATED:
-            logging.info("Enrollment Record Created")
+            logging.info("Record Created")
         return response
 
 
@@ -30,17 +31,17 @@ class EnrollmentRecordDetail(RetrieveUpdateDestroyAPIView):
     def retrieve(self, request, *args, **kwargs):
         response = super().retrieve(request, *args, **kwargs)
         if response.status_code == status.HTTP_200_OK:
-            logging.info("Enrollment Record Retrieved")
+            logging.info("Record Retrieved")
         return response
 
     def update(self, request, *args, **kwargs):
         response = super().update(request, *args, **kwargs)
         if response.status_code == status.HTTP_200_OK:
-            logging.info("Enrollment Record Updated")
+            logging.info("Record Updated")
         return response
 
     def destroy(self, request, *args, **kwargs):
         response = super().destroy(request, *args, **kwargs)
         if response.status_code == status.HTTP_204_NO_CONTENT:
-            logging.info("Enrollment Record Deleted")
+            logging.info("Record Deleted")
         return response
