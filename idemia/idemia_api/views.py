85244c197c44c57488650204e408afce2f136208
folksgl <Gfolks14@gmail.com>
2021-01-25 12:36:37 -0500

    include logging in application that shows points for future idemia calls

--- idemia/idemia_api/views.py
+++ idemia/idemia_api/views.py
@@ -16,9 +16,10 @@ class EnrollmentRecordCreate(CreateAPIView):
     serializer_class = EnrollmentRecordSerializer
 
     def create(self, request, *args, **kwargs):
+        """ Create an enrollment record. POST to idemia /pre-enrollments endpoint """
         response = super().create(request, *args, **kwargs)
         if response.status_code == status.HTTP_201_CREATED:
-            logging.info("Record Created")
+            logging.info("Record Created -- POST to idemia /pre-enrollments")
         return response
 
 
@@ -29,18 +30,22 @@ class EnrollmentRecordDetail(RetrieveUpdateDestroyAPIView):
     serializer_class = EnrollmentRecordSerializer
 
     def retrieve(self, request, *args, **kwargs):
+        """ Retrieve an enrollment record with the specified uuid """
         response = super().retrieve(request, *args, **kwargs)
         if response.status_code == status.HTTP_200_OK:
-            logging.info("Record Retrieved")
+            logging.info("Record Retrieved - GET on idemia /pre-enrollments/UEID")
+            logging.info("Call update() if status has changed")
         return response
 
     def update(self, request, *args, **kwargs):
+        """ Update an enrollment record """
         response = super().update(request, *args, **kwargs)
         if response.status_code == status.HTTP_200_OK:
             logging.info("Record Updated")
         return response
 
     def destroy(self, request, *args, **kwargs):
+        """ Delete an enrollment record """
         response = super().destroy(request, *args, **kwargs)
         if response.status_code == status.HTTP_204_NO_CONTENT:
             logging.info("Record Deleted")
