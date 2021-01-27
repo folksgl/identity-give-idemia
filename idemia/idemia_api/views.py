60e220292713268b4e3981492732fcd347ff5d50
folksgl <Gfolks14@gmail.com>
2021-01-27 11:20:54 -0500

    add initial transaction logging capability to /enrollment endpoint

--- idemia/idemia_api/views.py
+++ idemia/idemia_api/views.py
@@ -1,5 +1,8 @@
 """ Views for Idemia API """
 import logging
+import uuid
+import requests
+from django.conf import settings
 from rest_framework.generics import (
     CreateAPIView,
     RetrieveUpdateDestroyAPIView,
@@ -11,6 +14,35 @@ from .models import EnrollmentRecord
 from .serializers import EnrollmentRecordSerializer
 
 
+def log_transaction():
+    """ Log a transaction to the transaction logging microservice """
+    logging.info("Logging a transaction to /transaction")
+    if settings.DEBUG:
+        logging.debug("Skipping transaction logging while in debug mode")
+        return True  # Skip sending a transaction log in debug mode
+    try:
+        service_url = "https://transaction-log-terrific-fox-le.app.cloud.gov"
+        transaction_url = f"{service_url}/transaction/"
+        payload = {
+            "record_uuid": uuid.uuid4(),
+            "service_type": "PROOFING SERVICE",
+            "customer": "test_customer",
+            "csp": "test_csp",
+            "cost": 0,
+            "result": "test_result",
+        }
+
+        response = requests.post(transaction_url, data=payload)
+        logging.info(
+            "URL: %s\nresponse: %s", transaction_url, response.text,
+        )
+    except Exception as err:
+        logging.error("Error sending transaction log %s", err)
+        return False
+
+    return response.status_code == status.HTTP_201_CREATED
+
+
 class EnrollmentRecordCreate(CreateAPIView):
     """ Create EnrollmentRecord objects """
 
@@ -19,9 +51,12 @@ class EnrollmentRecordCreate(CreateAPIView):
 
     def create(self, request, *args, **kwargs):
         """ Create an enrollment record. POST to idemia /pre-enrollments endpoint """
-        response = super().create(request, *args, **kwargs)
-        if response.status_code == status.HTTP_201_CREATED:
-            logging.info("Record Created -- POST to idemia /pre-enrollments")
+        if log_transaction():
+            response = super().create(request, *args, **kwargs)
+            if response.status_code == status.HTTP_201_CREATED:
+                logging.info("Record Created -- POST to idemia /pre-enrollments")
+        else:
+            response = Response({"message": "Transaction logging failed. Aborting.."})
         return response
 
 
