cb124f98b9ad22e9118e37879a71ffbd1ce77204
folksgl <Gfolks14@gmail.com>
2021-01-28 18:23:31 -0500

    fix indentation

--- idemia/idemia_api/views.py
+++ idemia/idemia_api/views.py
@@ -59,82 +59,68 @@ def location_view(_request, zipcode):
     """ Exposes the /locations idemia UEP endpoint """
     logging.info("Calling Idemia /locations endpoint with zipcode: %s", zipcode)
 
-    # Dummy location info 
+    # Dummy location info
     location_list = [
-    {
-        "title": "IdentoGO - TSA PreCheck&#8482",
-        "address": "1 Saarinen Circle",
-        "address2": "IAD International Airport",
-        "city": "Sterling",
-        "state": "VA",
-        "postalCode": "20166-7547",
-        "distance": "10.452655457551472",
-        "hours": "Monday-Friday: 8:00 AM - 9:30 AM & 9:45 AM - 11:30 AM & 12:00 PM - 2:00 PM & 2:15 PM - 4:00 PM",
-        "phone":"855-787-2227",
-        "geocode": {
-            "latitude": "38.952809",
-            "longitude": "-77.447961"
-        }
-    },
-    {
-        "title": "IdentoGO TSA PreCheck&#8482 Enrollment at Staples",
-        "address": "8387 Leesburg Pike",
-        "address2": "Ste C",
-        "city": "Vienna",
-        "state": "VA",
-        "postalCode": "22182-2420",
-        "distance": "10.452655457551472",
-        "hours": "Monday-Friday: 10:00 AM - 12:00 PM & 1:00 PM - 5:00 PM",
-        "phone":"703-883-0011",
-        "geocode": {
-            "latitude": "38.921954",
-            "longitude": "-77.236917"
-        }
-    },
-    {
-        "title": "IdentoGO - TSA PreCheck&#8482, TWIC, HAZMAT",
-        "address": "1968 Gallows Rd",
-        "address2": "VA DMV-Tyson's Corner",
-        "city": "Vienna",
-        "state": "VA",
-        "postalCode": "22182-3909",
-        "distance": "20.51593994774416",
-        "hours": "Monday-Friday: 8:00 AM - 1:00 PM & 2:00 PM - 4:30 PM Saturday: 8:00 AM - 12:00 PM",
-        "phone":"807-497-7100",
-        "geocode": {
-            "latitude": "38.910709",
-            "longitude": "-77.225463"
-        }
-    },
-    {
-        "title": "IdentoGO TSA PreCheck&#8482 Enrollment at Staples",
-        "address": "9890 Liberia Ave",
-        "address2": "",
-        "city": "Manassas",
-        "state": "VA",
-        "postalCode": "20110-5836",
-        "distance": "24.29308762203185",
-        "hours": "Monday-Thursday: 10:00 AM - 12:00 PM & 1:00 PM - 6:00 PM",
-        "phone":"877-783-4187",
-        "geocode": {
-            "latitude": "38.743717",
-            "longitude": "-77.451883"
-        }
-    },
-    {
-        "title": "IdentoGO - State Agency Enrollment",
-        "address": "3139 Duke St",
-        "address2": "",
-        "city": "Alexandria",
-        "state": "VA",
-        "postalCode": "22314-4518",
-        "distance": "30.81106117961712",
-        "hours": "Monday-Thursday: 8:00 AM - 1:00 PM & 1:30 PM - 4:30 PM Friday: 8:00 AM - 1:00 PM & 1:30 PM - 4:00 PM",
-        "phone":"877-783-4187",
-        "geocode": {
-            "latitude": "38.808868",
-            "longitude": "-77.084946"
-        }
-    },
-]
-return Response(location_list)
+        {
+            "title": "IdentoGO - TSA PreCheck&#8482",
+            "address": "1 Saarinen Circle",
+            "address2": "IAD International Airport",
+            "city": "Sterling",
+            "state": "VA",
+            "postalCode": "20166-7547",
+            "distance": "10.452655457551472",
+            "hours": "Monday-Friday: 8:00 AM - 9:30 AM & 9:45 AM - 11:30 AM & 12:00 PM - 2:00 PM & 2:15 PM - 4:00 PM",
+            "phone": "855-787-2227",
+            "geocode": {"latitude": "38.952809", "longitude": "-77.447961"},
+        },
+        {
+            "title": "IdentoGO TSA PreCheck&#8482 Enrollment at Staples",
+            "address": "8387 Leesburg Pike",
+            "address2": "Ste C",
+            "city": "Vienna",
+            "state": "VA",
+            "postalCode": "22182-2420",
+            "distance": "10.452655457551472",
+            "hours": "Monday-Friday: 10:00 AM - 12:00 PM & 1:00 PM - 5:00 PM",
+            "phone": "703-883-0011",
+            "geocode": {"latitude": "38.921954", "longitude": "-77.236917"},
+        },
+        {
+            "title": "IdentoGO - TSA PreCheck&#8482, TWIC, HAZMAT",
+            "address": "1968 Gallows Rd",
+            "address2": "VA DMV-Tyson's Corner",
+            "city": "Vienna",
+            "state": "VA",
+            "postalCode": "22182-3909",
+            "distance": "20.51593994774416",
+            "hours": "Monday-Friday: 8:00 AM - 1:00 PM & 2:00 PM - 4:30 PM Saturday: 8:00 AM - 12:00 PM",
+            "phone": "807-497-7100",
+            "geocode": {"latitude": "38.910709", "longitude": "-77.225463"},
+        },
+        {
+            "title": "IdentoGO TSA PreCheck&#8482 Enrollment at Staples",
+            "address": "9890 Liberia Ave",
+            "address2": "",
+            "city": "Manassas",
+            "state": "VA",
+            "postalCode": "20110-5836",
+            "distance": "24.29308762203185",
+            "hours": "Monday-Thursday: 10:00 AM - 12:00 PM & 1:00 PM - 6:00 PM",
+            "phone": "877-783-4187",
+            "geocode": {"latitude": "38.743717", "longitude": "-77.451883"},
+        },
+        {
+            "title": "IdentoGO - State Agency Enrollment",
+            "address": "3139 Duke St",
+            "address2": "",
+            "city": "Alexandria",
+            "state": "VA",
+            "postalCode": "22314-4518",
+            "distance": "30.81106117961712",
+            "hours": "Monday-Thursday: 8:00 AM - 1:00 PM & 1:30 PM - 4:30 PM Friday: 8:00 AM - 1:00 PM & 1:30 PM - 4:00 PM",
+            "phone": "877-783-4187",
+            "geocode": {"latitude": "38.808868", "longitude": "-77.084946"},
+        },
+    ]
+
+    return Response(location_list)
