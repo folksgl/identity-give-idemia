af48993fbef97948ec704d47f05ebb07ba89dd39
folksgl <Gfolks14@gmail.com>
2021-02-07 16:58:40 -0500

    add comment to clarify script purpose

--- migrations.py
+++ migrations.py
@@ -8,6 +8,8 @@ from cfenv import AppEnv
 
 ENV = AppEnv()
 
+# Only allow the 0th instance of the application to run the migration scripts on the
+# database. When deploying there will always be at least 1 application instance.
 if ENV.index == 0:
     logging.warning("Instance index 0 started -- running migrations script")
     subprocess.call(["python", "manage.py", "migrate"])
