dffc4ba2feae11527be45089644c50f7ae689e27
pshumateEDC <pshumate@easydynamics.com>
2021-02-24 17:19:02 -0500

    Add PII to desired request body of the `POST /enrollment` endpoint in docs
    PII removed from the model caused the docs to be inaccurate. Added
    decorators to properly document required request body for the
    `POST /enrollment` endpoint.

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,22 @@
 """ Define URLs for the Django application """
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework import status
 from . import views
+from drf_yasg.utils import swagger_auto_schema
+from .serializers import EnrollmentRecordCreateSerializer, EnrollmentRecordSerializer
+
+decorated_enrollmentcreate_view = swagger_auto_schema(
+    method="post",
+    request_body=EnrollmentRecordCreateSerializer,
+    responses={status.HTTP_201_CREATED: EnrollmentRecordSerializer},
+)(views.EnrollmentRecordCreate.as_view())
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path("locations/<zipcode>", views.location_view, name="locations"),
-    path("enrollment/", views.EnrollmentRecordCreate.as_view(), name="enrollment"),
+    path("enrollment/", decorated_enrollmentcreate_view, name="enrollment"),
     path(
         "enrollment/<uuid:pk>",
         views.EnrollmentRecordDetail.as_view(),
