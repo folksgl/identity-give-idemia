93b44e220da6b0641dc54e01e4fee57d92f4eeae
folksgl <Gfolks14@gmail.com>
2021-03-01 15:08:41 -0500

    Update mechanism for running migrations
    Switch from using the Python subprocess module to using the
    "django.core.management" functions provided by Django. The recent use of
    Bandit called out the subprocess module as being a LOW security threat
    and the new method is more reliable in my opinion.

--- migrations.py
+++ migrations.py
@@ -3,14 +3,15 @@ migrations.py implements the 'Migrate Frequently' section of
 https://docs.huihoo.com/cloudfoundry/documentation/devguide/services/migrate-db.html
 """
 import logging
-import subprocess
 from cfenv import AppEnv
+from django.core.management import execute_from_command_line
 
 ENV = AppEnv()
+
 
 # Only allow the 0th instance of the application to run the migration scripts on the
 # database. When deploying there will always be at least 1 application instance.
 if ENV.index == 0:
     logging.warning("Instance index 0 started -- running migrations script")
-    subprocess.call(["python", "manage.py", "migrate"])
+    execute_from_command_line(["manage.py", "migrate"])
     logging.warning("Migrations complete")
