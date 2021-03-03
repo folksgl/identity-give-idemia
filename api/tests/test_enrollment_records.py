d0146243a94d4b6f01df50e4a20ef6490debad03
pshumateEDC <pshumate@easydynamics.com>
2021-03-03 14:11:52 -0500

    Update test cases for new model format
    Add headers to requests. Add new test for different CSP custom id.

--- api/tests/test_enrollment_records.py
+++ api/tests/test_enrollment_records.py
@@ -17,9 +17,9 @@ def generate_enrollment_record_data() -> dict:
     }
 
 
-def generate_meta_data() -> dict:
+def generate_meta_data(consumer_id) -> dict:
     """ Helper method for generating headers for all requests to the microservice """
-    return {"HTTP_HOST": "localhost"}
+    return {"HTTP_X_CONSUMER_CUSTOM_ID": consumer_id}
 
 
 def create_enrollment_record(client):
@@ -30,7 +30,7 @@ def create_enrollment_record(client):
     """
     url = reverse("enrollment")
     record_data = generate_enrollment_record_data()
-    meta_data = generate_meta_data()
+    meta_data = generate_meta_data("consumerA")
     response = client.post(url, record_data, **meta_data)
 
     return (response, record_data)
@@ -77,17 +77,27 @@ class EnrollmentRecordCRUDTest(APITestCase):
         _response, record_data = create_enrollment_record(self.client)
 
         url = reverse("enrollment-record", args=[record_data["record_csp_uuid"]])
-        meta_data = generate_meta_data()
+        meta_data = generate_meta_data("consumerA")
         get_response = self.client.get(url, **meta_data)
 
         self.assertEqual(get_response.status_code, status.HTTP_200_OK)
+
+    def test_get_enrollment_badheader(self):
+        """ Create a user, then test the 'get' operation on that user with a bad header """
+        _response, record_data = create_enrollment_record(self.client)
+
+        url = reverse("enrollment-record", args=[record_data["record_csp_uuid"]])
+        meta_data = generate_meta_data("consumerB")
+        get_response = self.client.get(url, **meta_data)
+
+        self.assertEqual(get_response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_delete_enrollment(self):
         """ Delete a user that was created """
         _response, record_data = create_enrollment_record(self.client)
 
         url = reverse("enrollment-record", args=[record_data["record_csp_uuid"]])
-        meta_data = generate_meta_data()
+        meta_data = generate_meta_data("consumerA")
         delete_response = self.client.delete(url, **meta_data)
         self.assertEqual(delete_response.status_code, status.HTTP_204_NO_CONTENT)
 
